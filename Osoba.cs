c3017abb3cac2c76d4cba583a9d41877c1d7bc8c
Paweł Bytner <pawel.m.bytner@gmail.com>
2023-02-26 15:15:33 +0100

    Dodaj pliki projektów.

---
+++ Osoba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_PR2_1_z2
+{
+    public class Osoba
+    {
+        public string Imię { get; set; } = "Nemo";
+    }
+}
